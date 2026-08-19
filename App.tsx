ed03a50fd23d762e750bd43058cb8711a8b338bf
Davyn <davyn.miller@hes-so.ch>
2026-08-19 10:26:24 +0300

    First screens creation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Movie Club</Text>
       <StatusBar style="auto" />
+      <View style={buttonStyles.Container}>
+        <Button title="Add Movie" onPress={() => {}} />
+        <Button title="View Movies" onPress={() => {}} />
+        <Button title="Remove Movie" onPress={() => {}} />
+      </View>
     </View>
   );
 }
 
+const buttonStyles = StyleSheet.create({
+  Container: {
+    flexDirection: "row",
+    gap: 10,
+    marginTop: 20,
+  },
+});
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
