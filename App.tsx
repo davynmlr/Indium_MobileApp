0c83de63307b4074b509479c4712e1dac1fb9d70
Davyn <davyn.miller@hes-so.ch>
2026-09-07 01:24:18 +0300

    fix dependencies + start of navig & screens

--- App.tsx
+++ App.tsx
@@ -1,27 +1,43 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Button } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import ProfileScreen from "./screens/ProfileScreen";
+import ActivityScreen from "./screens/ActivityScreen";
+import MoviesScreen from "./screens/MoviesScreen";
+import SearchScreen from "./screens/SearchScreen";
+import AddMovieScreen from "./screens/AddMovieScreen";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Movie Club</Text>
-      <StatusBar style="auto" />
-      <View style={buttonPageStyles.Container}>
-        <Button title="Add Movie" onPress={() => {}} />
-        <Button title="View Movies" onPress={() => {}} />
-        <Button title="Remove Movie" onPress={() => {}} />
-      </View>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{
+            tabBarLabel: "Profile",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="person" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Activity"
+          component={ActivityScreen}
+          options={{
+            tabBarLabel: "Activity",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="list" color={color} size={size} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const buttonPageStyles = StyleSheet.create({
-  Container: {
-    flexDirection: "row",
-    gap: 10,
-    marginTop: 20,
-  },
-});
 
 const styles = StyleSheet.create({
   container: {
---
+++ screens/SearchScreen.tsx
@@ -0,0 +1,18 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export default function SearchScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Search Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
